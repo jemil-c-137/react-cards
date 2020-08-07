cf203e27bb98f069e5225a008174641386703c66
Dzhemil Suleimanov <jemil1931@gmail.com>
2020-08-07 10:42:51 +0300

    added starting button

--- src/components/Wrapper/index.js
+++ src/components/Wrapper/index.js
@@ -1,39 +1,13 @@
 import React from "react";
 import CardsContainer from "../Card/CardContainer";
 import styles from './Wrapper.module.css'
-import RestartGame from "../NewGame";
-import {connect} from "react-redux";
-import {newGameAC, shuffleCardsAC} from "../../Redux/rootReducer";
 
 
-const Table = (props) => {
+const Table = () => {
   return(
     <div className={styles.container}>
-      {
-        props.gameOver ? <RestartGame {...props}/>
-        : <CardsContainer/>
-      }
+        <CardsContainer/>
     </div>
   )
 }
-
-const mapStateToProps = (state) => {
-  return {
-    gameOver: state.cardsPage.gameOver,
-    cards: state.cardsPage.cards
-  }
-}
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    shuffleCards: () => {
-      dispatch(shuffleCardsAC())
-    },
-    startNewGame: () => {
-      dispatch(newGameAC())
-    }
-  }
-
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Table)
+ export default Table
